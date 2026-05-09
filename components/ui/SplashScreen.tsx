c6358486a571c34c5fe91bce2569cfc0c9f83f05
Alex Nelson <nelsoncodes96@gmail.com>
2026-05-09 12:36:12 -0400

    refactor(SplashScreen): remove unneccesary useEffect previously used for overflow-y scroll lock

--- components/ui/SplashScreen.tsx
+++ components/ui/SplashScreen.tsx
@@ -1,20 +1,12 @@
 import Image from "next/image";
 import styles from "./SplashScreen.module.css";
 import clsx from "clsx";
-import { useEffect } from "react";
 
 export default function SplashScreen({
   splashPhase,
 }: {
   splashPhase: "animating" | "loading" | "ready";
 }) {
-  useEffect(() => {
-    // Removes inline style overflow-y: hidden set on body in layout.tsx - restores scroll on mount
-    return () => {
-      document.body.style.removeProperty("overflow-y");
-    };
-  }, []);
-
   return (
     <div
       className={clsx(
