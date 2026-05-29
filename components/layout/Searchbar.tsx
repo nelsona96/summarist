2e2eb75b913e79da9d8a82caaf5662f00424da83
Alex Nelson <nelsoncodes96@gmail.com>
2026-05-28 22:07:06 -0400

    style(searchbar): set max width to 1080px

--- components/layout/Searchbar.tsx
+++ components/layout/Searchbar.tsx
@@ -28,51 +28,53 @@ export default function Searchbar() {
 
   return (
     <div className={styles.wrapper}>
-      <form
-        onSubmit={handleSubmit}
-        role="search"
-        className={styles.inputWrapper}
-      >
-        <label htmlFor="search" className="srOnly">
-          Search for books
-        </label>
-        <input
-          onChange={handleInput}
-          value={input}
-          ref={inputRef}
-          id="search"
-          type="search"
-          placeholder="Search for books"
-          className={styles.input}
-        />
-        <div className={styles.searchIcons}>
-          {input && (
+      <div className={styles.content}>
+        <form
+          onSubmit={handleSubmit}
+          role="search"
+          className={styles.inputWrapper}
+        >
+          <label htmlFor="search" className="srOnly">
+            Search for books
+          </label>
+          <input
+            onChange={handleInput}
+            value={input}
+            ref={inputRef}
+            id="search"
+            type="search"
+            placeholder="Search for books"
+            className={styles.input}
+          />
+          <div className={styles.searchIcons}>
+            {input && (
+              <button
+                onClick={clearInput}
+                type="button"
+                aria-label="Clear search input"
+                className={styles.clearButton}
+              >
+                <LuX aria-hidden="true" />
+              </button>
+            )}
             <button
-              onClick={clearInput}
-              type="button"
-              aria-label="Clear search input"
-              className={styles.clearButton}
+              type="submit"
+              aria-label="Search"
+              className={styles.searchButton}
             >
-              <LuX aria-hidden="true" />
+              <LuSearch aria-hidden="true" />
             </button>
-          )}
-          <button
-            type="submit"
-            aria-label="Search"
-            className={styles.searchButton}
-          >
-            <LuSearch aria-hidden="true" />
-          </button>
-        </div>
-      </form>
-      <button
-        onClick={toggleSidebar}
-        aria-label="Toggle navigation"
-        aria-expanded={isOpen}
-        className={styles.menu}
-      >
-        <LuMenu aria-hidden="true" />
-      </button>
+          </div>
+        </form>
+        <button
+          onClick={toggleSidebar}
+          aria-label="Toggle navigation"
+          aria-expanded={isOpen}
+          className={styles.menu}
+        >
+          <LuMenu aria-hidden="true" />
+        </button>
+      </div>
     </div>
   );
 }
