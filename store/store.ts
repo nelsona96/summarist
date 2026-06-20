a9deb7741e82e063e34f0a39196e2adef74cb088
Alex Nelson <nelsoncodes96@gmail.com>
2026-06-20 12:54:31 -0400

    test: make redux store injectable for testing w/  helper functions, update affected test files

--- store/store.ts
+++ store/store.ts
@@ -1,16 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
 import authModalReducer from "./authModalSlice";
 import authReducer from "./authSlice";
 
-const store = configureStore({
-  reducer: {
-    authModal: authModalReducer,
-    auth: authReducer,
-  },
+const rootReducer = combineReducers({
+  authModal: authModalReducer,
+  auth: authReducer,
 });
 
-export type AppStore = typeof store;
+export function setupStore(preloadedState?: PreloadedState) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
+
+const store = setupStore();
+
+export type PreloadedState = Parameters<typeof rootReducer>[0];
 export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = typeof store;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
--- test/utils.tsx
+++ test/utils.tsx
@@ -1,11 +1,38 @@
-import React from "react";
-import { render, RenderOptions } from "@testing-library/react";
+import { render, type RenderOptions } from "@testing-library/react";
+import React, { PropsWithChildren } from "react";
+import { AppStore, PreloadedState, setupStore } from "@/store/store";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import authModalReducer from "@/store/authModalSlice";
 import authReducer from "@/store/authSlice";
 
-export * from "@testing-library/react";
+interface ExtendedRenderOptions extends Omit<
+  RenderOptions,
+  "queries" | "wrapper"
+> {
+  preloadedState?: PreloadedState;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  extendedRenderOptions: ExtendedRenderOptions = {},
+) {
+  const {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  } = extendedRenderOptions;
+
+  const Wrapper = ({ children }: PropsWithChildren) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+  };
+}
 
 export function makeStore() {
   return configureStore({
@@ -15,15 +42,3 @@ export function makeStore() {
     },
   });
 }
-
-function renderWithProviders(ui: React.ReactElement, options?: RenderOptions) {
-  const store = makeStore();
-
-  function Wrapper({ children }: { children: React.ReactNode }) {
-    return <Provider store={store}>{children}</Provider>;
-  }
-
-  return render(ui, { wrapper: Wrapper, ...options });
-}
-
-export { renderWithProviders as render };
