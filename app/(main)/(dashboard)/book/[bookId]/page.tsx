dd65d6d52d7353a974aa2cd15e78a1dd07d2d35a
Alex Nelson <nelsoncodes96@gmail.com>
2026-07-12 21:24:51 -0400

    feat: scaffold base html structure

--- app/(main)/(dashboard)/book/[bookId]/page.tsx
+++ app/(main)/(dashboard)/book/[bookId]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import styles from "./page.module.css";
 import { getBookById } from "@/lib/api";
 
 export default async function BookDetailsPage({
@@ -10,12 +12,48 @@ export default async function BookDetailsPage({
   const book = await getBookById(bookId);
 
   return (
-    <div>
-      <h1>{book.title}</h1>
-      <h2>{book.subTitle}</h2>
-      <p>{book.author}</p>
-      <br />
-      <p>{book.bookDescription}</p>
+    <div className={styles.container}>
+      <div className={styles.bookDetailsImgWrapper}>
+        <Image src={book.imageLink} alt={book.title} width={300} height={300} />
+      </div>
+      <div className={styles.bookDetails}>
+        <div>
+          <h1>{book.title}</h1>
+          <p>{book.author}</p>
+          <p>{book.subTitle}</p>
+        </div>
+
+        <ul>
+          <li>
+            {book.averageRating} ({book.totalRating} ratings)
+          </li>
+          <li>
+            {/* placeholder until future phase when real audio duration will be implemented: */}
+            01:23
+          </li>
+          <li>{book.type}</li>
+          <li>{book.keyIdeas} Key Ideas</li>
+        </ul>
+
+        <div>{/* buttons will go here */}</div>
+
+        {/* Add to library button goes here */}
+
+        <section aria-labelledby="book-description-heading">
+          <h2 id="book-description-heading">What's it about?</h2>
+          <ul>
+            {book.tags.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
+          <p>{book.bookDescription}</p>
+        </section>
+
+        <section aria-labelledby="author-description-heading">
+          <h2 id="author-description-heading">About the author</h2>
+          <p>{book.authorDescription}</p>
+        </section>
+      </div>
     </div>
   );
 }
