1b4ae9f6659d68cc036bff30f465c736991900dc
Alex Nelson <nelsoncodes96@gmail.com>
2026-07-23 17:34:10 -0400

    feat(ErrorFallback): refactor component for dynamic use with message and className props

--- components/errors/ErrorFallback.tsx
+++ components/errors/ErrorFallback.tsx
@@ -1,16 +1,23 @@
+import clsx from "clsx";
 import ErrorButton from "./ErrorButton";
 import styles from "./ErrorFallback.module.css";
 
 interface ErrorFallbackProps {
   onReset: () => void;
+  message: string;
+  className?: string | undefined;
 }
 
-export default function ErrorFallback({ onReset }: ErrorFallbackProps) {
+export default function ErrorFallback({
+  onReset,
+  message,
+  className,
+}: ErrorFallbackProps) {
   return (
     <main>
-      <div className={styles.container}>
+      <div className={clsx(styles.container, className)}>
         <div role="alert" className={styles.errorWrapper}>
-          <h1 className={styles.errorMessage}>Oops! Something went wrong.</h1>
+          <h1 className={styles.errorMessage}>{message}</h1>
           <ErrorButton onClick={onReset} />
         </div>
       </div>
