dc8ae2f010f747df47f17ef491d02f741db74545
Alex Nelson <nelsoncodes96@gmail.com>
2026-07-27 22:05:28 -0400

    feat: make ErrorButton and ErrorFallback components dynamic

--- components/errors/ErrorFallback.tsx
+++ components/errors/ErrorFallback.tsx
@@ -3,8 +3,9 @@ import ErrorButton from "./ErrorButton";
 import styles from "./ErrorFallback.module.css";
 
 interface ErrorFallbackProps {
-  onReset: () => void;
   message: string;
+  buttonLabel: string;
+  onReset: () => void;
   className?: string | undefined;
 }
 
@@ -12,15 +13,14 @@ export default function ErrorFallback({
   onReset,
   message,
   className,
+  buttonLabel,
 }: ErrorFallbackProps) {
   return (
-    <main>
-      <div className={clsx(styles.container, className)}>
-        <div role="alert" className={styles.errorWrapper}>
-          <h1 className={styles.errorMessage}>{message}</h1>
-          <ErrorButton onClick={onReset} />
-        </div>
+    <div className={clsx(styles.container, className)}>
+      <div role="alert" className={styles.errorWrapper}>
+        <h1 className={styles.errorMessage}>{message}</h1>
+        <ErrorButton onClick={onReset} label={buttonLabel} />
       </div>
-    </main>
+    </div>
   );
 }
