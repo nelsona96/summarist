4c08a1812265cebad0058ad86b117e39a83da25d
Alex Nelson <nelsoncodes96@gmail.com>
2026-08-05 15:53:57 -0400

    feat(not-found): added status code 404 heading for a11y

--- app/(main)/(dashboard)/book/[bookId]/not-found.tsx
+++ app/(main)/(dashboard)/book/[bookId]/not-found.tsx
@@ -5,6 +5,7 @@ export default function BookDetailsNotFound() {
   return (
     // TODO redirect to /search page in future when implemented, this is a temp config
     <NotFoundFallback
+      statusCode={404}
       message="Oops! This book is not in our catalog."
       buttonLabel="Search for Another Book"
       redirect="/for-you"
