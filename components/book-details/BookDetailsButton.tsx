e8ff323a94eba7972f4319c686e41199f310fb45
Alex Nelson <nelsoncodes96@gmail.com>
2026-08-27 20:43:51 -0400

    feat(BookDetailsButton): add logic to call requireAuth function when clicked if bookId present

--- components/book-details/BookDetailsButton.tsx
+++ components/book-details/BookDetailsButton.tsx
@@ -1,24 +1,48 @@
-import Button from "../ui/Button";
+"use client";
+
 import type { ButtonProps } from "@/types/button";
+import useRequireAuth from "@/hooks/useRequireAuth";
+import Button from "../ui/Button";
 
 type BookDetailsButtonProps = Pick<
   ButtonProps,
   "label" | "className" | "disabled" | "onClick"
 >;
 
+interface ButtonOnClick extends BookDetailsButtonProps {
+  onClick: () => void;
+  bookId?: never;
+}
+
+interface ButtonRedirect extends BookDetailsButtonProps {
+  bookId: string;
+  onClick?: never;
+}
+
 export default function BookDetailsButton({
+  bookId,
   label,
   className,
   disabled,
   onClick,
-}: BookDetailsButtonProps) {
+}: ButtonOnClick | ButtonRedirect) {
+  const requireAuth = useRequireAuth();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    } else if (bookId) {
+      requireAuth({ intent: "ACCESS_BOOK", payload: bookId });
+    }
+  };
+
   return (
     <Button
       type="button"
       variant="login"
       label={label}
       disabled={disabled}
-      onClick={onClick}
+      onClick={handleClick}
       className={className}
     />
   );
