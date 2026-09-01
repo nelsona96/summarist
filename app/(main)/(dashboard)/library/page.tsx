5fc4178657261e74fcb704fb936309fdada2419e
Alex Nelson <nelsoncodes96@gmail.com>
2026-08-31 23:01:00 -0400

    feat: add ui for logged out state with login to see library cta

--- app/(main)/(dashboard)/library/page.tsx
+++ app/(main)/(dashboard)/library/page.tsx
@@ -2,14 +2,19 @@
 
 import type { Book } from "@/types/book";
 import { useEffect, useState } from "react";
-import { useAppSelector } from "@/hooks/redux";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import { getBookById } from "@/lib/api";
 import styles from "./page.module.css";
 import BookCarousel from "@/components/book/BookCarousel";
 import Skeleton from "@/components/ui/Skeleton";
 import BookCarouselSkeleton from "@/components/book/BookCarouselSkeleton";
+import Button from "@/components/ui/Button";
+import { openModal } from "@/store/authModalSlice";
+import Image from "next/image";
+import clsx from "clsx";
 
 export default function Page() {
+  const { user } = useAppSelector((state) => state.auth);
   const bookIds = useAppSelector((state) => state.library);
   const [books, setBooks] = useState<Book[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -38,9 +43,12 @@ export default function Page() {
   }, [bookIds]);
 
   return (
-    <div>
-      <div>
-        <h1 className="srOnly">My Library</h1>
+    <>
+      <h1 className="srOnly">My Library</h1>
+
+      {!user ? (
+        <LoggedOutUi />
+      ) : (
         <section className={styles.section}>
           <div className="container">
             <h2 className={styles.sectionTitle}>Saved Books</h2>
@@ -58,7 +66,37 @@ export default function Page() {
             )}
           </div>
         </section>
+      )}
+    </>
+  );
+}
+
+function LoggedOutUi() {
+  const dispatch = useAppDispatch();
+
+  return (
+    <section className={styles.section}>
+      <div className={clsx("container", styles.loginContainer)}>
+        <Image
+          src="/assets/login.png"
+          alt="Login image"
+          width={460}
+          height={317}
+          className={styles.loginImg}
+        />
+
+        <p className={styles.loginPara}>
+          Log in to your account to see your library.
+        </p>
+
+        <Button
+          onClick={() => dispatch(openModal())}
+          variant="login"
+          type="button"
+          label="Login"
+          className={styles.button}
+        />
       </div>
-    </div>
+    </section>
   );
 }
