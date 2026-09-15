63222bb678acd4197e5206ed3e28fcf807865c96
Alex Nelson <nelsoncodes96@gmail.com>
2026-09-14 22:20:05 -0400

    feat: add loading spinner to /player route for both server and client side loading

--- components/player/PlayerLoading.tsx
+++ components/player/PlayerLoading.tsx
@@ -1,3 +1,10 @@
+import styles from "./PlayerLoading.module.css";
+import LoadingSpinner from "../ui/LoadingSpinner";
+
 export default function PlayerLoading() {
-  return <div>Loading...</div>;
+  return (
+    <div className={styles.spinner}>
+      <LoadingSpinner size="xxl" />
+    </div>
+  );
 }
